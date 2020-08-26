15d9e046ee02ac91570352e0770c2bfc53b1f26a
uther <minchul.kim@chai.finance>
2020-08-26 15:24:02 +0900

    landscape 화면 추가.

--- app/src/main/java/kr/minchulkim/tichucalculator/presentation/MainActivity.kt
+++ app/src/main/java/kr/minchulkim/tichucalculator/presentation/MainActivity.kt
@@ -1,11 +1,12 @@
 package kr.minchulkim.tichucalculator.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.fragment.app.activityViewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -13,10 +14,12 @@ import kr.minchulkim.tichucalculator.R
 import kr.minchulkim.tichucalculator.databinding.MainActivityBinding
 import kr.minchulkim.tichucalculator.viewmodel.GameListVM
 
+
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private var keepScreenOn: Boolean = false
     private val gameListVM: GameListVM by viewModels()
     private lateinit var binding: MainActivityBinding
 
@@ -40,19 +43,47 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        if (keepScreenOn) {
+            menu.findItem(R.id.keep_screen_toggle).setIcon(R.drawable.ic_baseline_highlight_24)
+        } else {
+            menu.findItem(R.id.keep_screen_toggle).setIcon(R.drawable.ic_outline_highlight_24)
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.clear_all) {
-            MaterialAlertDialogBuilder(this)
-                .setMessage(R.string.delete_all_message)
-                .setPositiveButton(R.string.delete) { _, _ ->
-                    gameListVM.onClickClear()
-                }
-                .setNegativeButton(R.string.cancel) { _, _ ->
-                    //no-op
-                }
-                .show()
+            showClearAlert()
+            return true
+        } else if (item.itemId == R.id.keep_screen_toggle) {
+            toggleKeepScreenOn()
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showClearAlert() {
+        MaterialAlertDialogBuilder(this)
+            .setMessage(R.string.delete_all_message)
+            .setPositiveButton(R.string.delete) { _, _ ->
+                gameListVM.onClickClear()
+            }
+            .setNegativeButton(R.string.cancel) { _, _ ->
+                //no-op
+            }
+            .show()
+    }
+
+    private fun toggleKeepScreenOn() {
+        val newFlag = !keepScreenOn
+        if (newFlag) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+            Toast.makeText(this, R.string.keep_screen_on_toast, Toast.LENGTH_SHORT).show()
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+        keepScreenOn = newFlag
+        invalidateOptionsMenu()
     }
 }
