845ab2743f8fafa1f70208febeca9652b7eea45e
Uther <uther@chai.finance>
2020-08-30 18:30:39 +0900

    fix build.gradle, delete unused gradle dependency

--- app/src/main/java/kr/minchulkim/tichucounter/TichuCounter.kt
+++ app/src/main/java/kr/minchulkim/tichucounter/TichuCounter.kt
@@ -2,7 +2,6 @@ package kr.minchulkim.tichucounter
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
 class TichuCounter : Application() {
